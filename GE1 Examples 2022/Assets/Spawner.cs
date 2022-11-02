d0e49afb9fbac3ef1c8af0127186731229a34f0b
cilly <C19491104@mytudublin.ie>
2022-11-02 13:49:24 +0000

    added spawning falling tanks

--- GE1 Examples 2022/Assets/Spawner.cs
+++ GE1 Examples 2022/Assets/Spawner.cs
@@ -4,26 +4,27 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-
+    public int count = 0;
+    public GameObject g;
     System.Collections.IEnumerator Spawn()
     {
-        while(true)
+        if (count < 5)
         {
-            GameObject g = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            Vector3 randomSpawnPosition = new Vector3(Random.Range(-3, 13), 5, Random.Range(-8, 8));
             g.AddComponent<Rigidbody>();
+            Instantiate(g, randomSpawnPosition, Quaternion.identity);
             g.transform.position = transform.position;
             yield return new WaitForSeconds(.2f);
+            count = count + 1;
         }
+   
     }
     // Start is called before the first frame update
-    void Start()
-    {
-        StartCoroutine(Spawn());
-    }
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        StartCoroutine(Spawn());
     }
 }
